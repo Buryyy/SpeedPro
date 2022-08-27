dae2615d3ae4ca2757ee85aa6512b49d03b0ea58
king.nakama <king.nakama@hotmail.com>
2022-08-27 03:33:57 +0300

    Device scanning functionality with BLE.

--- SpeedPro/Platforms/Android/MainActivity.cs
+++ SpeedPro/Platforms/Android/MainActivity.cs
@@ -1,10 +1,67 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.App;
+using Activity = Android.App.Activity;
 
 namespace SpeedPro;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+
+    public static Activity Activity { get; private set; }
+    public static Context Context { get; private set; }
+    private static readonly string[] BLE_PERMISSIONS = new string[]{
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation,
+    };
+
+    private static readonly string[] ANDROID_12_BLE_PERMISSIONS = new string[] {
+            Manifest.Permission.BluetoothScan,
+            Manifest.Permission.BluetoothConnect,
+            Manifest.Permission.AccessFineLocation,
+    };
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        Activity = this;
+        Context = this;
+        //RequestBlePermissions(this, 0);
+        base.OnCreate(savedInstanceState);
+    }
+
+    public static bool IsBLEAccessGranted()
+    {
+        if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.S)
+        {
+            foreach (var permission in ANDROID_12_BLE_PERMISSIONS)
+            {
+                if (AndroidX.Core.Content.ContextCompat.CheckSelfPermission(Context, permission) == Android.Content.PM.Permission.Denied)
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            foreach (var permission in BLE_PERMISSIONS)
+            {
+                if (AndroidX.Core.Content.ContextCompat.CheckSelfPermission(Context, permission) == Android.Content.PM.Permission.Denied)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void RequestBlePermissions(Activity activity, int requestCode)
+    {
+        if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.S)
+            ActivityCompat.RequestPermissions(activity, ANDROID_12_BLE_PERMISSIONS, requestCode);
+        else
+            ActivityCompat.RequestPermissions(activity, BLE_PERMISSIONS, requestCode);
+    }
 }
--- SpeedPro/Services/BluetoothLEService.cs
+++ SpeedPro/Services/BluetoothLEService.cs
@@ -6,9 +6,9 @@ namespace SpeedPro.Services
 {
     public class BluetoothLEService : IBluetoothLEService
     {
-        private const string UUID1 = "00008888-0000-1000-8000-00805f9b34fb";
-        private const string UUID2 = "00008877-0000-1000-8000-00805f9b34fb";
-        private const string UUID_Des = "00002902-0000-1000-8000-00805f9b34fb";
+        private readonly string[] PossibleUUIDs = new string[]{ "00008888-0000-1000-8000-00805f9b34fb",
+        "00008877-0000-1000-8000-00805f9b34fb",
+        "00002902-0000-1000-8000-00805f9b34fb"};
 
         private readonly IBluetoothLE _bluetooth;
         private readonly IAdapter _adapter;
@@ -18,12 +18,10 @@ namespace SpeedPro.Services
         {
             _scannedDevices = new List<IDevice>();
             _bluetooth = CrossBluetoothLE.Current;
-            _bluetooth.StateChanged += OnStateChanged;
             _adapter = CrossBluetoothLE.Current.Adapter;
+            _bluetooth.StateChanged += OnStateChanged;
             _adapter.DeviceDiscovered += (s, a) =>
             {
-
-                System.Diagnostics.Debug.WriteLine("Discovered device: " + a.Device.Name);
                 _scannedDevices.Add(a.Device);
             };
         }
@@ -33,35 +31,19 @@ namespace SpeedPro.Services
             Console.WriteLine(e.NewState.ToString());
         }
 
+
         public async Task ScanDevicesAsync()
         {
+
             _scannedDevices.Clear();
-
+#if ANDROID
+if(!SpeedPro.MainActivity.IsBLEAccessGranted()) {
+SpeedPro.MainActivity.RequestBlePermissions(SpeedPro.MainActivity.Activity, 0);
+}
+#endif
             await _adapter.StartScanningForDevicesAsync();
+
             //await _adapter.DiscoverDeviceAsync(new Guid("00001101-0000-1000-8000-00805F9B34FB"));
-
-        }
-
-        private async Task ValidateBluetoothPermission()
-        {
-
-
-        }
-
-        public async Task<bool> ConnectDeviceAsync(IDevice device)
-        {
-
-            return true;
-        }
-
-        public void SendPacket(byte[] payload)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task ConnectAsync(string deviceName)
-        {
-
         }
 
         ~BluetoothLEService()
@@ -69,5 +51,6 @@ namespace SpeedPro.Services
             _adapter.DeviceDiscovered -= (s, a) => _scannedDevices.Add(a.Device);
             _bluetooth.StateChanged -= OnStateChanged;
         }
+
     }
 }
